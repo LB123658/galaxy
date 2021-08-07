b263705ea8c2eba57e5c9cfd46ae8a95f1065b27
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-08-07 14:17:06 -0500

    Update quick.js

--- quick.js
+++ quick.js
@@ -302,10 +302,6 @@ quick.style.visibility = "visible";
 } else if (text.search("near me") > -1) {
 getLocation();
 quick.style.visibility = "visible";
-} else if (text.search(" ") == -1) {
-first.innerHTML = text + ".com - suggested website<br><a href='https://" + text + ".com' title='This link has not been verified and may be unsafe'>" + text + ".com</a>";
-quick.innerHTML = "<b>Galaxy is working on having better results</b><br><p style='text-align:center;'><span class='material-icons' style='color:#5765f2;'>feedback</span></p>The first result has not been checked and may not actually lead to a website.";
-quick.style.visibility = "visible";
 } else {
 quick.style.visibility = "hidden";
 }
