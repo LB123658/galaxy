05842afaad03bedd4e28bb3512675ecc11a17e05
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-08-07 16:04:07 -0500

    Update info.js

--- info.js
+++ info.js
@@ -1,5 +1,5 @@
 var info = document.getElementById("info");
-info.innerHTML = "The 2020 Tokyo Olympics are going on right now. <a href='https://lb123658.github.io/galaxy/search?q=2020+Tokyo+Olympics'>Learn more</a>";
+info.innerHTML = "Get the new Galaxy browser extension. <a href='https://lb123658.github.io/galaxy/extension/extension.zip'>Download here</a>";
 if (window.location.search == "?source=blocked_page") {
 info.innerHTML = "Galaxy blocks searches it doesn't think are safe.";
 }
