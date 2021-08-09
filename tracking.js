a38e191f69c927058fdd6d4ed409f83fbf3cdf7e
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-08-09 18:00:02 -0500

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -2,4 +2,7 @@ const d = new Date();
 var searchHistory = localStorage.getItem("search_history");
 var addedHtml = "<a href='" + window.location.href + "' target='_blank'><button class='history'>" + location.search.split("=")[1].split("&")[0].replace(/\+/gi, " ") + " - Galaxy Search <span style='float:right;'>" + d.toLocaleTimeString() + "</span></button></a>";
 
-document.body.onload = function() { localStorage.setItem("search_history",searchHistory + addedHtml); };
+function trackSearch() {
+localStorage.setItem("search_history",searchHistory + addedHtml); 
+}
+trackSearch();
