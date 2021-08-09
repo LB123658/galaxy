a41144085a23d448101ca567720563011ad4ed6e
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-08-09 13:01:51 -0500

    Update notification.js

--- notification.js
+++ notification.js
@@ -1,6 +1,6 @@
 function displayNotification(text) {
 var css = document.createElement("style");
-css.innerHTML = "#banner { position: absolute; top: 15px; right: 15px; width: 250px; z-index: 10; background: #5765f2; color: white; padding: 10px; border-radius: 8px; animation-name: slide; animation-duration: 1s; cursor: pointer; user-select: none; transition: transform 0.5s; }#banner:hover {transform: scale(1.1);}@keyframes slide { 0% {right: -270px;} 100% {right: 15px;} }";
+css.innerHTML = "#banner { position: absolute; top: 15px; right: 15px; width: 250px; z-index: 10; background: #5765f2; color: white; padding: 10px; border-radius: 8px; animation-name: slide; animation-duration: 1s; cursor: pointer; user-select: none; transition: transform 0.5s; text-align: center; }#banner:hover {transform: scale(1.1);}@keyframes slide { 0% {right: -270px;} 100% {right: 15px;} }";
 document.head.appendChild(css);
 var banner = document.createElement("div");
 banner.id = "banner";
