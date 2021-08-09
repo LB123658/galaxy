81bc62bb47dd8908b72be95611984626dfabf462
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-08-09 17:24:15 -0500

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,3 @@
+const d = new Date();
+var searchHistory = localStorage.getItem("search_history");
+var addedHtml = "<a href='" + window.location.href + "' target='_blank'><button class='history'>" + location.search.split("=")[1].split("&")[0].replace(/\+/gi, " ") + " - Galaxy Search <span style='float:right;'>" + d.toLocaleTimeString() + "</span></button></a>";
