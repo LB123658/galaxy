bad1d84d9ae00ed62c6b79a2f2df501d0b1ee6a3
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-08-11 13:48:38 -0500

    Update icon-bar.js

--- m/icon-bar.js
+++ m/icon-bar.js
@@ -1,11 +1,14 @@
 function iconBar() {
-var iconbarStyle = "#bottom-icon-row { position: fixed; bottom: 0px; left: 0px; width: 100%; height: 200px; overflow: hidden; background: #e8e4e3; z-index: 2; } #bottom-icon-row button { width: 33.3%; float: left; background: transparent; border: none; height: 400px; margin-top: -100px; border-radius: 60%; transition: transform 0.5s; } #bottom-icon-row button:hover { background: lightgrey; transform: scale(1.2); } #bottom-icon-row button:focus { background: lightgrey; transform: scale(1.2); }";
+var iconbarStyle = "#bottom-icon-row { position: fixed; bottom: 0px; left: 0px; width: 100%; height: 200px; overflow: hidden; background: #e8e4e3; z-index: 2; background: white; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.39);} #bottom-icon-row button { width: 33.3%; float: left; background: transparent; border: none; height: 400px; margin-top: -100px; border-radius: 60%; transition: transform 0.5s; } #bottom-icon-row button:hover { background: #c7cbf0; transform: scale(1.2); } #bottom-icon-row button:focus { background: #c7cbf0; transform: scale(1.2); }";
 var css = document.createElement("style");
 css.innerHTML = iconbarStyle;
 document.head.appendChild(css);
 var bottomiconRow = document.createElement("div");
 bottomiconRow.id = "bottom-icon-row";
-bottomiconRow.innerHTML = "<button id='home'><span class='material-icons' style='color:black;font-size:40px;'>home</span></button> <button id='history'><span class='material-icons' style='color:black;font-size:40px;'>history</span></button> <button id='more'><span class='material-icons' style='color:black;font-size:40px;'>more_vert</span></button>";
+bottomiconRow.innerHTML = "<a href='https://lb123658.github.io/galaxy/m'><button id='home'><span class='material-icons' style='color:black;font-size:80px;'>home</span></button></a> <a href='https://lb123658.github.io/galaxy/m/history'><button id='history'><span class='material-icons' style='color:black;font-size:80px;'>history</span></button></a> <button id='more' onclick='share()'><span class='material-icons' style='color:black;font-size:80px;'>more_vert</span></button>";
 document.body.appendChild(bottomiconRow);
 }
 iconBar();
+function share() {
+navigator.share(window.location.href);
+}
